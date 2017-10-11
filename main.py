f8505236bc1389d189daff6359b778d06eb03393
Eric Berger <eric@brrgrr.com>
2017-10-11 10:22:01 -0500

    Error handling

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,7 +7,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:XfoLalnS9rAh7u6K@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-# app.secret_key = 'aI*oJ*RoF&U9VIueEb4aK@Dc!6IYq58o'
+app.secret_key = 'aI*oJ*RoF&U9VIueEb4aK@Dc!6IYq58o'
 
 
 class Blog(db.Model):
@@ -32,23 +32,29 @@ def blog():
     blog_id = request.args.get('id')
     if blog_id == None:
         blogs = Blog.query.all()
-        return render_template('blog.html', title="Build-a-Blog", blogs=blogs)
+        return render_template('blog.html', page_title="Build-a-Blog", blogs=blogs)
     else:
         post = Blog.query.get(blog_id)
-        return render_template('post.html', title="Build-a-Blog: " + post.title, post=post)
+        return render_template('post.html', page_title="Build-a-Blog: " + post.title, post=post)
 
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def newpost():
-    if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
-        new_blog = Blog(title, body)
-        db.session.add(new_blog)
-        db.session.commit()
-        return redirect('/blog?id=' + str(new_blog.id))
-    else:
-        return render_template('newpost.html', title="Build-a-Blog: Add A New Entry")
+    if request.method == 'GET':
+        return render_template('newpost.html', page_title="Build-a-Blog: Add A New Entry")
+    elif request.method == 'POST':
+        title = request.form['title'].strip()
+        body = request.form['body'].strip()
+        if not title or not body:
+            # error_check = True
+            flash("You must fill out all fields to create a new post.", 'error')
+            return render_template('newpost.html', page_title="Build-a-Blog: Add A New Entry", title=title, body=body)
+        else:
+            new_blog = Blog(title, body)
+            db.session.add(new_blog)
+            db.session.commit()
+            flash('New post, '+ new_blog.title + ', was added at ' + str(new_blog.pub_date), 'success')
+            return redirect('/blog?id=' + str(new_blog.id))
 
 
 if __name__ == '__main__':
