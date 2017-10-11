3559c6b8b4d2f28d6abbfbe5167b8df90a0a1f6d
Eric Berger <eric@brrgrr.com>
2017-10-11 12:42:31 -0500

    Update db and key

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:XfoLalnS9rAh7u6K@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:HM1m34mIk7IdwoRH@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-app.secret_key = 'aI*oJ*RoF&U9VIueEb4aK@Dc!6IYq58o'
+app.secret_key = 'V076143I8P1Unt&5%KTRkwJCuY@EgsdU'
 
 
 class Blog(db.Model):
